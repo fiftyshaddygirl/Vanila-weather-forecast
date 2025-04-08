354fe0c480627a81af7350b84559c679a08a1718
cynkzn <cynkazan@gmail.com>
2025-04-08 18:48:23 +1000

    update weather icon

--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ function refreshWeather(response) {
   let time = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   time.innerHTML = formatDate(date);
+
+  let icon = document.querySelector("#icon");
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}">`;
+
+  console.log(response.data);
 }
 
 function formatDate(date) {
